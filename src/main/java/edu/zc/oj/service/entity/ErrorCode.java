05e91b38bd5d7ad9f1e9f870fb9e69a56fa0f4b4
coderPlus-tr <523906@qq.com>
2021-03-02 14:51:15 +0800

    feat: add enum ResultCode ErrorCode

---
+++ src/main/java/edu/zc/oj/service/entity/ErrorCode.java
@@ -0,0 +1,28 @@
+package edu.zc.oj.service.entity;
+
+public enum ErrorCode {
+    /**
+     * result entity member error code
+     */
+    SUCCESS(0),
+    INVALID_CONFIG(-1),
+    FORK_FAILED(-2),
+    PTHREAD_FAILED(-3),
+    WAIT_FAILED(-4),
+    ROOT_REQUIRED(-5),
+    LOAD_SECCOMP_FAILED(-6),
+    SETRLIMIT_FAILED(-7),
+    DUP2_FAILED(-8),
+    SETUID_FAILED(-9),
+    EXECVE_FAILED(-10),
+    SPJ_ERROR(-11);
+    private final Integer value;
+
+    ErrorCode(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
