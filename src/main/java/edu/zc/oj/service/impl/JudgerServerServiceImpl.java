f2cde39049fb526cd814be93c70724e0b4b3b606
keeplooking6 <1422910966@qq.com>
2021-03-02 23:59:19 +0800

    feat: add args'transJson,transObject

---
+++ src/main/java/edu/zc/oj/service/impl/JudgerServerServiceImpl.java
@@ -0,0 +1,34 @@
+package edu.zc.oj.service.impl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.zc.oj.service.JudgerServerService;
+
+
+public class JudgerServerServiceImpl implements JudgerServerService {
+
+//    序列化
+    private static String transJson(Object object){
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+        try {
+            json = objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+//反序列化
+    private static <T> T transObject(String json, Class<T> clazz){
+        ObjectMapper objectMapper = new ObjectMapper();
+        T object = null;
+        try {
+            object = objectMapper.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+
+}
