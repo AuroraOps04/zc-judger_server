c80c566287f3e80ad435c0c9b232b32712aceacc
coderPlus-tr <523906@qq.com>
2021-03-08 14:54:18 +0800

    chore: replace swagger3 with swagger2

--- src/main/java/edu/zc/oj/configuration/SwaggerConfig.java
+++ src/main/java/edu/zc/oj/configuration/SwaggerConfig.java
@@ -5,36 +5,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.ArrayList;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author keep-looking
  * @date 2021/3/6 - 14:47
  */
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     /**
      * Select Swagger's enforceable environment and configure which interfaces to scan
+     *
      * @param environment
      * @return
      */
 
     @Bean
-    public Docket docket(Environment environment){
+    public Docket docket(Environment environment) {
         Profiles dev = Profiles.of("dev");
         boolean flag = environment.acceptsProfiles(dev);
-
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiinfo())
+                .apiInfo(apiInfo())
                 .pathMapping("/")
-                .groupName("zc-judger_server")
+                .groupName("zc-judge_server")
                 .enable(flag)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("edu.zc.oj.controller"))
@@ -44,19 +45,15 @@ public class SwaggerConfig {
 
     /**
      * author info
+     *
      * @return
      */
-    private ApiInfo apiinfo() {
+    private ApiInfo apiInfo() {
         Contact contact = new Contact("陶大锐和王晓玲", "https://www.cnblogs.com/c-aha", "5239604@qq.com");
-        return new ApiInfo(
-                "zc-judger_server的API文档",
-                "什么都是从0到1",
-                "v1.0",
-                "https://www.cnblogs.com/c-aha",
-                contact,
-                "Apache 2.0",
-                "http://www.apache.org/licenses/LICENSE-2.0",
-                new ArrayList<>()
-        );
+        return new ApiInfoBuilder()
+                .title("zc-judge_server的API文档")
+                .version("1.0")
+                .contact(contact)
+                .build();
     }
 }
